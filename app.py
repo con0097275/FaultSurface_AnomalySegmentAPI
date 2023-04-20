3fff6cd354b214790b2b89f4d00881a10601194f
Nguyen Ke Van <van00972756@gmail.com>
2023-04-20 17:30:00 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+
+
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+from ImageNotes import ImageNote
+import numpy as np
+import pickle
+import pandas as pd
+# from main import TypePrediction, segment_image,saveResult
+from main  import predictImage
+import datetime
+import json
+import requests
+
+# 2. Create the app object
+app = FastAPI()
+
+# 3. Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+# 4. Route with a single parameter, returns the parameter within a message
+#    Located at: http://127.0.0.1:8000/AnyNameHere
+@app.get('/{name}')
+def get_name(name: str):
+    return {'Welcome To KeVan Youtube Channel': f'{name}'}
+
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted Bank Note with the confidence
+@app.post('/predict')
+def predict_crack(data:ImageNote):
+    data = data.dict()
+    img=data['image']
+    result= predictImage(img)
+    # pred= TypePrediction(img)
+    # image_base64= segment_image(img)
+
+    # # print(pred)
+    # if(pred>0.5):
+    #     type="Have crack"
+    # else:
+    #     type="Have no crack"
+
+    # result= {
+    #     'date': str(datetime.datetime.now()),
+    #     'original_image':img,
+    #     'prediction': pred,
+    #     'type': type,
+    #     'segment_image': image_base64
+    # }
+
+    # print(result)
+    # # saveResult(result)     ## save input and output result in mongodb database Atlas
+    # url = "https://ap-southeast-1.aws.data.mongodb-api.com/app/data-wlatu/endpoint/data/v1/action/insertOne"
+    # payload = json.dumps({
+    # "collection": "fault_detection",
+    # "database": "thesis",
+    # "dataSource": "Cluster0",
+    # "document": {'date':result['date'], 'original_image': result['original_image'], 'prediction': result['prediction'],'type':result['type'], 'segment_image':result['segment_image']}
+    # })
+    # headers = {
+    # 'Content-Type': 'application/json',
+    # 'Access-Control-Request-Headers': '*',
+    # 'api-key': 'LFyT8MWcEraGxtCsMJpceBO8q72WLX8mInon25j6kbVCgv2j5vSwVYzNVzdxFsqh', 
+    # }
+
+    # response = requests.request("POST", url, headers=headers, data=payload)
+    # print(response.text)
+    
+    return {
+        'prediction': str(result['prediction']),
+        'type': result['type'],
+        'image': result['segment_image']
+    }
+
+    # return {
+    #     'prediction': str(pred)
+    # }
+
+
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload
---
+++ main.py
@@ -0,0 +1,225 @@
+
+import numpy as np
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+from keras.models import load_model
+import urllib.request
+from utils import load_unet_vgg16, load_unet_resnet_101, load_unet_resnet_34
+from tqdm import tqdm
+from os.path import join
+from pathlib import Path
+import cv2 as cv
+import torch.nn.functional as F
+from torch.autograd import Variable
+from unet.unet_transfer import UNet16, input_size
+import torchvision.transforms as transforms
+import base64
+import io
+import os
+import matplotlib.pyplot as plt
+import glob
+import requests
+import json
+import torch
+import datetime
+
+#Load the trained model. (Pickle file)
+classify_model = load_model('./Models/VGG16_batchsize=32_epo=10.h5')
+
+def TypePrediction(im_file):
+    SIZE = 150 
+    # im_bytes = base64.b64decode(my_string)   # im_bytes is a binary image
+    # im_file = io.BytesIO(im_bytes)  # convert image to file-like object
+    # im_file=Image.open(im_file)
+    img = np.asarray(im_file.resize((SIZE,SIZE)))
+    
+    # img_path="https://lptech.asia/uploads/files/2020/07/10/seo-hinh-anh-la-gi-lptech.png"
+    # urllib.request.urlretrieve(img_path, "PredictImage")
+    # img = np.asarray(Image.open("PredictImage").resize((SIZE,SIZE)))
+    
+    img = img/255.      #Scale pixel values
+    
+    img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
+    
+    pred = classify_model.predict(img) #Predict    
+    return round(pred[0][0],4)
+
+
+# model predict segmentation:
+    
+##load model:
+model_path ="Models/model_unet_vgg_16_best.pt"
+model_type='vgg16'   
+if model_type == 'vgg16':
+    model = load_unet_vgg16(model_path)
+elif model_type  == 'resnet101':
+    model = load_unet_resnet_101(model_path)
+elif model_type  == 'resnet34':
+    model = load_unet_resnet_34(model_path)
+    print(model)
+else:
+    print('undefind model name pattern')
+    exit()
+    
+def evaluate_img(model, img):
+    input_width, input_height = input_size[0], input_size[1]
+    img_height, img_width, img_channels = img.shape
+    img_1 = cv.resize(img, (input_width, input_height), cv.INTER_AREA)
+    channel_means = [0.485, 0.456, 0.406]
+    channel_stds  = [0.229, 0.224, 0.225]
+    train_tfms = transforms.Compose([transforms.ToTensor(), transforms.Normalize(channel_means, channel_stds)])
+
+    X = train_tfms(Image.fromarray(img_1))
+    # X = Variable(X.unsqueeze(0)).cuda()  # [N, 1, H, W]
+    X = Variable(X.unsqueeze(0)).cpu()
+
+    mask = model(X)
+
+    mask = torch.sigmoid(mask[0, 0]).data.cpu().numpy()
+    mask = cv.resize(mask, (img_width, img_height), cv.INTER_AREA)
+    return mask
+
+
+def find_name():
+    arr_img=glob.glob("./test_imgs/*")
+    
+    arr_img=list(map(lambda x: x.rsplit("\\",1)[-1] ,arr_img))
+    arr_num= list(map(int,filter(lambda t: t.isnumeric(),map(lambda x: x.split(".",1)[0] ,arr_img))))
+    if (len(arr_img)==0):
+        return "1.jpg"
+    
+    file_name= str(max(arr_num)+1) + ".jpg"
+    return file_name
+
+
+    # #encode:
+    # with open(join(*[out_viz_dir, f'{path.stem}.jpg']), "rb") as img_file:
+    #     my_string = base64.b64encode(img_file.read())
+
+def segment_image(im_file):
+    
+
+    img_dir= 'test_imgs'
+    
+    out_viz_dir='test_results'
+    out_pred_dir='test_imgs_pred'
+    # threshold =0.2
+
+    # #test
+    # with open(join(img_dir, "crack_in_large_context_01.jpg"), "rb") as img_file:
+    #     my_string = base64.b64encode(img_file.read())
+
+    if out_viz_dir != '':
+        os.makedirs(out_viz_dir, exist_ok=True)
+    if out_pred_dir != '':
+        os.makedirs(out_pred_dir, exist_ok=True)
+
+    
+    # my_string=bytes(my_string,'utf-8') #convert input string to byte:
+    # #decode to Pil Image:
+    # im_bytes = base64.b64decode(my_string)   # im_bytes is a binary image
+    # im_file = io.BytesIO(im_bytes)  # convert image to file-like object
+    # img_0 = Image.open(im_file)
+    filename= find_name()  #file name to save result
+    img_0=im_file
+    img_0.save(os.path.join(img_dir,filename))   ## save input image
+    img_0 = np.asarray(img_0)
+    
+
+
+    if len(img_0.shape) != 3:
+        print('incorrect image shape : must have 3 channels')
+    img_0 = img_0[:,:,:3]
+    img_height, img_width, img_channels = img_0.shape
+    
+
+    prob_map_full = evaluate_img(model, img_0)
+
+
+    ##save image mask:
+    plt.imsave(os.path.join(out_pred_dir,filename), prob_map_full, cmap='gray')
+
+
+    plt.clf()
+    plt.imshow(img_0)
+    plt.imshow(prob_map_full, alpha=0.4)
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.gcf().set_size_inches(img_width/100, img_height/100)    
+    plt.savefig(os.path.join(out_viz_dir,filename),dpi=100) 
+
+    #encode:
+    with open(os.path.join(out_viz_dir,filename), "rb") as img_file_t:
+        output = base64.b64encode(img_file_t.read())
+    # print(output.decode("utf-8"))
+
+    return output.decode("utf-8")
+
+
+
+def saveResult(result):
+    url = "https://ap-southeast-1.aws.data.mongodb-api.com/app/data-wlatu/endpoint/data/v1/action/insertOne"
+    payload = json.dumps({
+        "collection": "fault_detection",
+        "database": "thesis",
+        "dataSource": "Cluster0",
+        "document": {'date':result['date'], 'original_image': result['original_image'], 'prediction': result['prediction'],'type':result['type'], 'segment_image':result['segment_image']}
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'LFyT8MWcEraGxtCsMJpceBO8q72WLX8mInon25j6kbVCgv2j5vSwVYzNVzdxFsqh', 
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+
+def predictImage(img):
+    my_string=bytes(img,'utf-8') #convert input string to byte:
+    #decode to Pil Image:
+    im_bytes = base64.b64decode(my_string)   # im_bytes is a binary image
+    im_file = io.BytesIO(im_bytes)  # convert image to file-like object
+    im_file= Image.open(im_file)
+
+    pred= TypePrediction(im_file)
+    segment_img= segment_image(im_file)
+
+    # print(pred)
+    if(pred>0.5):
+        type="Crack"
+    else:
+        type="Normal"
+    result= {
+        'date': str(datetime.datetime.now()),
+        'original_image':img,
+        'prediction': float(pred),
+        'type': type,
+        'segment_image': segment_img
+    }
+    saveResult(result)
+    return result
+
+
+
+# model = load_model("./Models/VGG16_batchsize=32_epo=10.h5")
+# def getPrediction(img_path):
+#     urllib.request.urlretrieve(img_path, "PredictImage")
+    
+#     SIZE=120
+#     a=[]
+#     img = np.asarray(Image.open("PredictImage").convert('L').resize((SIZE,SIZE)))
+#     a.append(img)
+#     a=np.array(a)
+#     a=a.reshape(-1, SIZE,SIZE, 1)
+#     a=a/255    
+    
+#     pred = model.predict(a)[0][0] #Predict    
+#     return pred
+
+
+
+
+
+# test_prediction =getPrediction('https://phuongnamcons.vn/wp-content/uploads/2021/06/vet-nut-tuong-nho.jpg')
+# print(test_prediction)
+
